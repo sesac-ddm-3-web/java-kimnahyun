5fa45adde5f59d83b6d30aa32315fbdc4e3209e2
nahyun0423 <skgus0423@naver.com>
2025-12-02 01:18:06 +0900

    remove

--- src/main/java/com/project/notice/infrastructure/MemberJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.notice.infrastructure;
-
-import com.project.notice.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberJpaRepository extends JpaRepository<Member, Long> {
-    Member findByUsername(String username);
-}
--- src/main/java/com/project/notice/infrastructure/PostJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.notice.infrastructure;
-
-import com.project.notice.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostJpaRepository extends JpaRepository<Post, Long> {
-}
